61e52f59bbde9d2dd06649a9ec9bf352187b2e81
Carabalone <guilherme.caraba@gmail.com>
2024-07-12 16:43:52 -0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+local enable_providers = {
+      "python3_provider",
+      "node_provider",
+      -- and so on
+    }
+    
+    for _, plugin in pairs(enable_providers) do
+      vim.g["loaded_" .. plugin] = nil
+      vim.cmd("runtime " .. plugin)
+    end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
